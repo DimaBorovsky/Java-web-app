f3f7393dde7b0985bef2864a3cbdb6264ddd527c
DimaBorovsky <dimab1143@gmail.com>
2026-02-23 13:21:08 +0200

    proviosn ec2 files

--- tf-config/main.tf
+++ tf-config/main.tf
@@ -4,13 +4,30 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.81.0"
     }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "1.23.0"
+    }
   }
 }
-resource "aws_ami" "ami" {
-  name = "my_ami"
+data "aws_ami" "ami" {
+  most_recent = true
 }
 
 resource "aws_instance" "tf-test" {
-  ami = aws_ami.ami.name
+  ami = data.aws_ami.ami.id
   instance_type = "t3.micro"
+
 }
+
+resource "aws_security_group" "allow_ssh" {
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+

219d47fae9e6ffa5383f435e09c7e5bf894010f0
DimaBorovsky <dimab1143@gmail.com>
2026-02-24 11:59:20 +0200

    added new output of AZ made a change to use AMI ssm and removed IP also adde a provider block to use locally stored credentials and config

--- tf-config/main.tf
+++ tf-config/main.tf
@@ -3,20 +3,26 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.81.0"
-    }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = "1.23.0"
+
     }
   }
 }
-data "aws_ami" "ami" {
-  most_recent = true
+
+provider "aws" {
+  shared_config_files = ["/Users/dima.borovsky/.aws/config"]
+  shared_credentials_files = ["/Users/dima.borovsky/.aws/credentials"]
 }
 
+
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+
 resource "aws_instance" "tf-test" {
-  ami = data.aws_ami.ami.id
+  ami  = data.aws_ssm_parameter.amazon_linux.value
   instance_type = "t3.micro"
+
 
 }
 
@@ -25,9 +31,8 @@ resource "aws_security_group" "allow_ssh" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["x.x.x.x/x"] # update your IP as necessary/IP of company VPN etc...
   }
-
 }
 
 
